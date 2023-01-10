193a5de0536c37ca5047f63b234d1a0fb2ed1ef0
SanderPaasalu <s.paasalu@gmail.com>
2023-01-10 12:13:52 +0200

    Update main.js
    Added back start function and started storying enemy stats

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
+let score = 0;
 let enemy1 = new Enemy();
-// function to test console log values to see if they are what they should be and change as needed
-function consoleLog(){
-    newEnemy();
-    console.log('Player stats: \n HP: ' + currentHP + ' attackPower: ' + attackPower + ' mana: ' + mana + ' spellPower: ' + spellPower);
-    console.log('Enemy stats: \n HP: ' + enemy1.HP + ' attackPower: ' + enemy1.attackP + ' mana: ' + enemy1.manaAmount + ' spellPower: ' + enemy1.SpellPower);
-    console.log();
-    console.log();
-    console.log();
-}
-// function to make enemy
 
+// function for starting the game
+function start(){
+    console.log('I have been called in HTML')
+    var startGame = document.getElementById('start');
+    var chooseClass = document.getElementById('class');
+    chooseClass.style.display = "block";
+    startGame.style.display = "none";
+}
+
+// function to make enemy and store their default stat values
+let enemyHp;
+let enemyAttack;
+let enemyMana;
+let enemySpellP
 function newEnemy(){
     enemy1.setAttack(50);
     enemy1.setHealth(100);
     enemy1.setMana(50);
     enemy1.setSpellPower(50);
+    enemyHp = enemy1.HP;
+    enemyAttack = enemy1.attackP;
+    enemyMana = enemy1.manaAmount;
+    enemySpellP = enemy1.spellPower;
+}
+
+// function to test console log values to see if they are what they should be and change as needed
+function consoleLog(){
+    newEnemy();
+    console.log('Player stats: \n HP: ' + currentHP + ' attackPower: ' + attackPower + ' mana: ' + mana + ' spellPower: ' + spellPower);
+    console.log('Enemy stats: \n HP: ' + enemyHp+ ' attackPower: ' + enemyAttack + ' mana: ' + enemyMana + ' spellPower: ' + enemySpellP);
+    console.log();
+    console.log();
+    console.log();
 }
